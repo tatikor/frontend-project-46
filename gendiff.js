19327141315806af4f3bd2aefd54c3be8c5acfb7
tatikor <acerbity@mail.ru>
2024-04-07 21:27:19 +0300

    finish step1

--- gendiff.js
+++ gendiff.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-const program = new Command();
+import { program } from 'commander';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .option('-v, --version', 'output the version number')
   .option('-h, --help', 'output usage information')
-  
+  if (process.argv.includes('-h') || process.argv.includes('--help')) {
+    program.outputHelp();
+  }
 program.parse();
