c5d14aaddc5316b2a92fb4097c574a7c763bbcae
Tatiana Korotkova <acerbity@mail.ru>
2024-04-07 19:53:06 +0300

    start project, add gendiff, add commander

---
+++ gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-v, --version', 'output the version number')
+  .option('-h, --help', 'output usage information')
+  
+program.parse();
